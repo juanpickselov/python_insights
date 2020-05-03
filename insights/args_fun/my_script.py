e0a35fe55e5ca1e56f8b74d0f5079281b9a32767
juanpickselov <juanpickselov@sbcglobal.net>
2020-05-03 14:59:27 -0400

    adding my insights based on work stuff

---
+++ insights/args_fun/my_script.py
@@ -0,0 +1,12 @@
+import sys
+
+
+def do_something(start_at=1, stop_at=2):
+    print('Start:', start_at, 'Stop:', stop_at)
+
+
+if __name__ == '__main__':
+    do_something(*sys.argv[1:])
+    print('Script Name:= ', sys.argv[0])
+    print('Arg 1:', sys.argv[1])
+    print('Arg 2:', sys.argv[2])
