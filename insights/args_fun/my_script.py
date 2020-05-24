834abee28ee836ed87a3f38f00162baba5480db0
juanpickselov <juanpickselov@sbcglobal.net>
2020-05-23 21:32:12 -0400

    setup black as an external tool

--- insights/args_fun/my_script.py
+++ insights/args_fun/my_script.py
@@ -5,8 +5,8 @@ def do_something(start_at=1, stop_at=2):
     return start_at, stop_at
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     do_something(*sys.argv[1:])
-    print('Script Name:= ', sys.argv[0])
-    print('Arg 1:', sys.argv[1])
-    print('Arg 2:', sys.argv[2])
+    print("Script Name:= ", sys.argv[0])
+    print("Arg 1:", sys.argv[1])
+    print("Arg 2:", sys.argv[2])
